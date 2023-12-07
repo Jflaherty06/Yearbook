738c6c027845af16c3ad8c00b2ad39fe4eea2795
John Flaherty <jflaherty5106@stu.d214.org>
2023-12-07 11:16:29 -0600

    Initial Commit

---
+++ Yearbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yearbook
+//
+//  Created by Jak Flaherty on 12/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yearbook/YearbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  YearbookApp.swift
+//  Yearbook
+//
+//  Created by Jak Flaherty on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct YearbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
