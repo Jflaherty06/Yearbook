48aa543570f89fa2d166189734e95cb9c91a36f0
Jak Flaherty <jflaherty5106@stu.d214.org>
2024-01-19 10:30:10 -0600

    created choose yearbook photo view

---
+++ Yearbook/ChoosePhoto.swift
@@ -0,0 +1,21 @@
+//
+//  ChoosePhoto.swift
+//  Yearbook
+//
+//  Created by Jak Flaherty on 1/19/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ChoosePhoto: View {
+    var body: some View {
+        Text("Placeholder")
+    }
+}
+
+#Preview {
+    ChoosePhoto()
+}
+
+
