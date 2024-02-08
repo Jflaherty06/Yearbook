d314b3d353acd5e412e33c2b5c5202965bcf33f8
Carinagatta <cgatta6130@stu.d214.org>
2024-02-08 11:04:25 -0600

    Button in check view

--- Yearbook/CheckView.swift
+++ Yearbook/CheckView.swift
@@ -12,22 +12,36 @@ struct CheckView: View {
     @State var StudentID = ""
     @State var purchased:Bool = true
     var body: some View {
-        TextField("Enter Student ID", text: $StudentID)
-        NavigationLink(destination: BuyView()) {
-            Text("Go to buy yearbook")
+        GeometryReader { geometry in
+           
+            VStack{
+                TextField("Enter Student ID", text: $StudentID)
+                    .frame(maxWidth: 600, maxHeight: 20)
+                    .font(.system(size: 30))
+                    .padding()
+                    
+                NavigationLink(destination: BuyView()) {
+                    Text("Go to buy yearbook")
+                        .frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.1)
+                }
+                Button {
+                    //            if StudentID == {
+                    //                purchased = true
+                    //                StudentID = ""
+                    //                Text("you have purchased a yearbook")
+                    //                .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
+                    //            } else if {
+                    //                purchased = false
+                    //                StudentID = ""
+                    //                Text("you have not purchased a yearbook")
+                    //                .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
+                    //            NavigationLink("Buy a yearbook", destination: BuyView())
+                    //            }
+                } label: {
+                    Text("check")
+                }
+            }
         }
-//        Button {
-//            if StudentID == {
-//                purchased = true
-//                StudentID = ""
-//            } else if {
-//                purchased = false
-//                StudentID = ""
-//            }
-//        } label: {
-//            Text("check")
-//        }
-
     }
    
 }
