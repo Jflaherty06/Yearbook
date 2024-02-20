4ab5c72e88485f2cb1192984fca947aefc7a52c0
Jak Flaherty <jflaherty5106@stu.d214.org>
2024-02-20 11:08:06 -0600

    succesfully pulled from firebase and got funciton working

--- Yearbook/CheckView.swift
+++ Yearbook/CheckView.swift
@@ -16,85 +16,53 @@ struct CheckView: View {
     @State var StudentID = ""
     @State var purchased:Bool = true
     @State var text = ""
+    @State var text1 = ""
     var body: some View {
-        VStack{
+        VStack {
             TextField("Enter Student ID", text: $StudentID)
             NavigationLink(destination: BuyView()) {
                 Text("Go to buy yearbook")
             }
             
             Button {
+                var intID = Int(StudentID)
                 var answer = checkID(studentID: StudentID)
+                if answer == false{
+                    text1 = "false"
+                }
                 if answer == true {
                     text = "You have not purchased a yearbook"
-                } else{
+                } else if answer == false {
                     text = "You have purchased a yearbook, thank you!"
                 }
                 
             }
-    label: {
-        Text("Check ID")
-    }
-    .onSubmit {
-        StudentID = ""
-        
-    }
-        
-        Text(text)
-    }
-}
-
-
-
-
-
-    func checkID(studentID: String) -> Bool{
-        for number in viewModel.idNumbers{
-            if studentID == number {
-                return true
-            }
+        label: {
+            Text("Check ID")
+        }
+        .onSubmit {
+            
+            
+        }
+            
+            Text(text)
+            Text(text1)
+            
         }
     }
-//=======
-//        GeometryReader { geometry in
-//           
-//            VStack{
-//                TextField("Enter Student ID", text: $StudentID)
-//                    .frame(maxWidth: 600, maxHeight: 20)
-//                    .font(.system(size: 30))
-//                    .padding()
-//                    
-//                NavigationLink(destination: BuyView()) {
-//                    Text("Go to buy yearbook")
-//                        .frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.1)
-//                }
-//                Button {
-//                    //            if StudentID == {
-//                    //                purchased = true
-//                    //                StudentID = ""
-//                    //                Text("you have purchased a yearbook")
-//                    //                .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
-//                    //            } else if {
-//                    //                purchased = false
-//                    //                StudentID = ""
-//                    //                Text("you have not purchased a yearbook")
-//                    //                .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
-//                    //            NavigationLink("Buy a yearbook", destination: BuyView())
-//                    //            }
-//                } label: {
-//                    Text("check")
-//                }
-//            }
-//>>>>>>> main
-//        }
-//    }
-//    return false
-//}
-//
-//}
-
-#Preview {
-    CheckView()
+    
+    
+    
+    func checkID(studentID: String) -> Bool{
+        for number in viewModel.idNumbers{
+            var num = number
+            if number == studentID {
+               return true
+            }
+        }
+        return false
+    }
 }
+
 
 
