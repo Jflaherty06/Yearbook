2eba880fc19d302e753638c94564dbf2d1a654e6
Jak Flaherty <jflaherty5106@stu.d214.org>
2024-02-22 10:47:11 -0600

    made new view that kind of just testes out a different way of showing a website in our app

---
+++ WebView.swift
@@ -0,0 +1,24 @@
+//
+//  WebView.swift
+//  Yearbook
+//
+//  Created by Jak Flaherty on 2/22/24.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
--- Yearbook/BuyView.swift
+++ Yearbook/BuyView.swift
@@ -7,24 +7,28 @@
 
 import Foundation
 import SwiftUI
+
 struct BuyView: View {
+    
+    @State var isPresentWebView = false
+    
     var body: some View {
-        GeometryReader { geometry in
-            Link(destination: URL(string: "https:www.infinitecampus.com")!, label: {
-                Text("infinite campus")
-            }) .font(.system(size: 60))
-                .font(.title)
-                .foregroundStyle(.green)
-                .shadow(radius: 20)
-            //        .environment(\.openURL, OpenURLAction(handler: handleURL))
-        } .padding()
-    }
-//    func handleURL(_ url: URL) -> OpenURLAction.Result {
-//          
-//           return .handled
-//       }
-            
+        
+        VStack {
+            Button("Open Webview") {
+                isPresentWebView  = true
+            }.sheet(isPresented: $isPresentWebView) {
+                NavigationStack {
+                    WebView(url: URL(string: "https://d214il.infinitecampus.org/campus/portal/students/township_214.jsp?status=logoff")!)
+                        .ignoresSafeArea()
+                        .navigationTitle("Infinite Campus")
+                        .navigationBarTitleDisplayMode(.inline)
+                    Text("--------------------------------\nTo buy a yearbook do this\nThen do this\nAnd finally do this")
+                }
+            }
         }
+    }
+}
 
 
 #Preview {
