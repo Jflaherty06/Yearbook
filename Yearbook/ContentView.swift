adf208cdaf207886a7d9783b9f106f0ce35a026c
Thomas  Niezyniecki <tniezyniecki6291@stu.d214.org>
2024-03-05 11:11:12 -0600

    added button for popout view with info abt yearbook

--- Yearbook/ContentView.swift
+++ Yearbook/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     @State var x = 0.0
     @State var y = 0.0
-    @State var  instructions = ""
+    @State var  instructions = "The Hersey yearbook is a great thing\nIt has so many awesome pictures\nIt features many different sports and clubs"
     var body: some View {
         
         GeometryReader { geometry in
@@ -21,9 +21,8 @@ struct ContentView: View {
                         .resizable()
                         .frame(width: geometry.size.width * 1.1, height: geometry.size.height * 1.7)
                         .ignoresSafeArea()
-                    VStack{
-                        HStack{
-                            
+                    VStack {
+                        HStack {
                             VStack {
                                 Text("2024 Yearbook Wrapped")
                                     .font(.system(size: 60))
@@ -54,32 +53,38 @@ struct ContentView: View {
                                         .foregroundColor(.black)
                                         .background(RoundedRectangle(cornerRadius: 20.0).fill(.white))
                                     }
-//                                    NavigationLink(destination: ChoosePhotoView()) {
-//                                        HStack {
-//                                            Text(Image(systemName: "play.circle"))
-//                                            Text("Choose Yearbook Photo")
-//                                        }
-//                                        .font(.system(size: geometry.size.height * 0.02))
-//                                        .padding()
-//                                        .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 0.1)
-//                                        .foregroundColor(.black)
-//                                        .background(RoundedRectangle(cornerRadius: 20.0).fill(.white))
-//                                    }
+                                    //                                    NavigationLink(destination: ChoosePhotoView()) {
+                                    //                                        HStack {
+                                    //                                            Text(Image(systemName: "play.circle"))
+                                    //                                            Text("Choose Yearbook Photo")
+                                    //                                        }
+                                    //                                        .font(.system(size: geometry.size.height * 0.02))
+                                    //                                        .padding()
+                                    //                                        .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 0.1)
+                                    //                                        .foregroundColor(.black)
+                                    //                                        .background(RoundedRectangle(cornerRadius: 20.0).fill(.white))
+                                    //                                    }
                                 }
                                 .padding()
                             }
                             .padding()
-//                            ZStack{
-//                                Button("<---") {
-//                                    instructions = "The Hersey yearbook is a great thing\nIt has so many awesome pictures\nIt features many different sports and clubs"
-//                                    x = 400
-//                                    y = .infinity
-//                                }
-//                                VStack{
-//                                    Text(instructions)
-//                                }
-//                                .frame(width: x, height: y)
-//                            }
+                            VStack(alignment: .trailing) {
+                                HStack {
+                                    Button {
+                                        x = 400
+                                        y = geometry.size.height * 1
+                                    } label: {
+                                        Text("\(Image(systemName: "arrowshape.left.fill"))")
+                                            .font(.system(size: 20))
+                                            .foregroundColor(.orange)
+                                    }
+                                    VStack {
+                                        Text(instructions)
+                                            .padding()
+                                    }
+                                    .frame(width: x, height: y)
+                                }
+                            }
                         }
                     }
                 }
