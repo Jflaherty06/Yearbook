a074d9a916cdd05b0e7290f3d141c99245a9174f
jhhsxcode <jhhs.xcode@d214.org>
2024-04-04 10:43:59 -0500

    made vip photos view visible

--- Yearbook/ChoosePhotoView.swift
+++ Yearbook/ChoosePhotoView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ChoosePhotoView: View {
     var body: some View {
+        
         GeometryReader { geometry in
+            VStack {
             Text("Placeholder")
                 .font(.system(size: 60))
                 .font(.title)
@@ -18,12 +20,18 @@ struct ChoosePhotoView: View {
                 .shadow(radius: 20)
                 .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
             NavigationStack{
-                VipPhotosWebView(url: URL(string: "https://www.vipis.com/group/9bb336c8/john-hersey-high-school")!)
-                    .frame(width: geometry.size.width * 75, height: geometry.size.height * 0.4)
-                    .border(.bar)
+                HStack{
+                    Spacer()
+                    
+                    VipPhotosWebView(url: URL(string: "https://www.vipis.com/group/9bb336c8/john-hersey-high-school")!)
+                        .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.7)
+                        .border(.black)
+                    
+                }
             }
         }
     }
+}
 }
 #Preview {
     ChoosePhotoView()
