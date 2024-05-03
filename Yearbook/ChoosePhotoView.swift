a23582e28ffdb52fc6fe9c52d42412e9894b630a
Carinagatta <cgatta6130@stu.d214.org>
2024-05-03 11:06:20 -0500

    did the instructions

--- Yearbook/ChoosePhotoView.swift
+++ Yearbook/ChoosePhotoView.swift
@@ -15,11 +15,11 @@ struct ChoosePhotoView: View {
                 Color.gray
                     .ignoresSafeArea()
             VStack() {
-                Text("Buy a Yearbook")
+                Text("Choose a Yearbook")
                     .font(.system(size: 40))
-                Text(" 1.To buy a yearbook, click on the link to get to Infinite Campus.")
-                Text("2. Then, go to your fees and add a Yearbook to it ($50).")
-                Text("Once you've paid, your fees will show the 50 dollar amount. This does not mean you have to pay again.")
+                Text(" 1. Click the School Portraits button")
+                Text("2. Then, Enter your information and click search")
+                Text("Once you've done that, you should be able to look at your photos and chose the one you would like to use for the yearbook.")
                     .padding()
                     .border(.orange, width: 6)
                     .font(.subheadline)
