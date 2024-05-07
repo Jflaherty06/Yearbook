7a06e5e520915e70d14f9a344e8d32da4860acbd
Jak Flaherty <jflaherty5106@stu.d214.org>
2024-05-07 11:40:33 -0500

    THIS IS THE FINAL WORKING VERSION MR. BROWN

--- Yearbook/Preview Content/CheckViewModel.swift
+++ Yearbook/Preview Content/CheckViewModel.swift
@@ -23,17 +23,17 @@ class CheckViewModel: ObservableObject {
     }
     
     func PullFromFirebase(){
-        let databaseRef = Database.database().reference().child("1hC3j_RPiYrvseXdg5RaFWgFkCpS8nqMl2uGkChfcY2c").child("yearbook_fees")
-        databaseRef.getData { myError, myDataSnapshot in
-            print(myDataSnapshot)
-            var newList: [String] = []
-            for idNumber in myDataSnapshot?.children.allObjects as! [DataSnapshot] {
-                let number = idNumber.key
-//                let dictionary = idNumber.value as! [Int:Any]
-                newList.append(number)
-            }
-            self.idNumbers = newList
-        }
+//        let databaseRef = Database.database().reference().child("1hC3j_RPiYrvseXdg5RaFWgFkCpS8nqMl2uGkChfcY2c").child("yearbook_fees")
+//        databaseRef.getData { myError, myDataSnapshot in
+//            print(myDataSnapshot)
+//            var newList: [String] = []
+//            for idNumber in myDataSnapshot?.children.allObjects as! [DataSnapshot] {
+//                let number = idNumber.key
+////                let dictionary = idNumber.value as! [Int:Any]
+//                newList.append(number)
+//            }
+//            self.idNumbers = newList
+//        }
     }
 
     
