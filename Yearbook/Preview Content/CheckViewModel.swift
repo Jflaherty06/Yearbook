2e851e95812c9a6f72ac34bdb5f84cd416f26d63
Thomas  Niezyniecki <tniezyniecki6291@stu.d214.org>
2024-05-09 09:56:42 -0500

    update check view model

--- Yearbook/Preview Content/CheckViewModel.swift
+++ Yearbook/Preview Content/CheckViewModel.swift
@@ -29,7 +29,6 @@ class CheckViewModel: ObservableObject {
             var newList: [String] = []
             for idNumber in myDataSnapshot?.children.allObjects as! [DataSnapshot] {
                 let number = idNumber.key
-//                let dictionary = idNumber.value as! [Int:Any]
                 newList.append(number)
             }
             self.idNumbers = newList
@@ -39,21 +38,3 @@ class CheckViewModel: ObservableObject {
     
     
 }
-    
-    
-    
-    
-//            func PullFromFirebase(){
-//                let databaseRef = Database.database().reference().child("1hC3j_RPiYrvseXdg5RaFWgFkCpS8nqMl2uGkChfcY2c").child("yearbook_fees")
-//                databaseRef.getData { myError, myDataSnapshot in
-//                    var newList: [String] = []
-//                    for idNumber in myDataSnapshot?.children.allObjects as! [DataSnapshot] {
-//                        let number = idNumber.key
-//        //                let dictionary = idNumber.value as! [Int:Any]
-//                        newList.append(number)
-//                    }
-//                    self.idNumbers = newList
-//                }
-//            }
-    
-
