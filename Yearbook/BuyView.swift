a2e0682602962b72d54d060214ce622dd0da50d2
Thomas  Niezyniecki <tniezyniecki6291@stu.d214.org>
2024-05-17 11:08:45 -0500

    uncommented buy view and photo view and chnaged photo view web view.

--- Yearbook/BuyView.swift
+++ Yearbook/BuyView.swift
@@ -25,10 +25,6 @@ struct BuyView: View {
                     Text("Once you've paid, your fees will show the 50 dollar amount. This does not mean you have to pay again.")
                         .padding()
                         .fontWeight(.bold)
-//                        .border(Color(red: 252/255.0, green: 103/255.0, blue: 26/255.0), width: 6)
-//                        .font(.subheadline)
-//                        .foregroundStyle(.white)
-//                        .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
                     NavigationStack {
                         InfiniteCampusWebView(url: URL(string: "https://d214il.infinitecampus.org/campus/portal/students/township_214.jsp?status=logoff")!)
                             .frame(width: geometry.size.width * 1, height: geometry.size.height * 0.80)
--- Yearbook/ChoosePhotoView.swift
+++ Yearbook/ChoosePhotoView.swift
@@ -22,17 +22,14 @@ struct ChoosePhotoView: View {
                 Text("2. Then, Enter your information and click search")
                 Text("Once you've done that, you should be able to look at your photos and chose the one you would like to use for the yearbook.")
                     .padding()
-                    .border(.orange, width: 6)
-                    .font(.subheadline)
-                    .foregroundStyle(.white)
-                    .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.1)
+                    .fontWeight(.bold)
                 NavigationStack {
                     let url = URL(string: "https://www.vipis.com/group/9bb336c8/john-hersey-high-school") ?? URL(string: "https://www.apple.com")
                     VipPhotosWebView(url: url!)
+                        .frame(width: geometry.size.width * 1, height: geometry.size.height * 0.80)
+                    
+                    InfiniteCampusWebView(url: URL(string: "https://www.vipis.com/group/9bb336c8/john-hersey-high-school")!)
                         .frame(width: geometry.size.width * 0.85, height: geometry.size.height * 0.75)
-
-//                    InfiniteCampusWebView(url: URL(string: "https://www.vipis.com/group/9bb336c8/john-hersey-high-school")!)
-                        
                     
                 }
             }
